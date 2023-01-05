b428512eb92d89ca4bdb4ceefe03851add53390f
Garrett <garrett@localhost>
2023-01-04 18:47:12 -0800

    Added PrusaFilamentChange script

---
+++ PrusaFilamentChange.py
@@ -0,0 +1,99 @@
+# -*- encoding: utf-8 -*-
+
+# Heavily based on Cura's FilamentChange.py (inside Cura app bundle on macOS)
+# Used this blog post for info, since Ultimaker's docs were down:
+# https://ryanbuhl.com/ryanbuhl.com/2020/09/19/cura-post-processing-scripts-part-iii-insertatlayerchange-initialization-and-user-settings/
+
+import json
+import io
+import string
+from ..Script import Script
+from UM.Logger import Logger
+
+class PrusaFilamentChange(Script):
+    def getSettingDataString(self):
+        return json.dumps({
+            "name": "Prusa Filament Change",
+            "key": "PrusaFilamentChange",
+            "metadata": {},
+            "version": 2,
+            "settings": {
+                "enable": {
+                    "label": "Enable",
+                    "description": "When enabled, will write settings at end of gcode",
+                    "type": "bool",
+                    "default_value": True
+                },
+                "layer": {
+                    "label": "Layer",
+                    "description": "At what layer should filament change occur. This will be before the layer starts printing.",
+                    "type": "int",
+                    "default_value": 1,
+                    "minimum_value": 1,
+                    "minimum_value_warning": "1",
+                    "enabled": "enable"
+                },
+                "finalchange": {
+                    "label": "Change after final layer",
+                    "description": "Add a final filament change at the end of the print.",
+                    "type": "bool",
+                    "default_value": False,
+                    "enabled": "enable"
+                }
+            }
+        })
+
+    def execute(self, data):
+        Logger.log("d", "Executing PrusaFilamentChange now")
+        """Adds Prusa-friendly filament change G-code at the specific layer
+
+        :param data: A list of layers of g-code.
+        :return: A similar list, with filament change commands inserted.
+        """
+        enable = self.getSettingValueByKey("enable")
+        layer = self.getSettingValueByKey("layer") + 1
+        finalchange = self.getSettingValueByKey("finalchange")
+
+        if not enable:
+            Logger.log("d", "Skipping filament change (disabled).")
+            return data
+
+        if not 0 < layer < len(data):
+            Logger.logException("w", "Layer index out of range.")
+            return data
+
+        color_change = """
+
+
+; Filament change code by PrusaFilamentChange
+G60 S2 ; Save current print position
+G1 X220 Y-3.0 ; Go outside print area
+M600 ; Filament change
+M0 Remove extrusion then continue ; Pause the print
+G1 R2 ; Return to saved print position
+
+
+"""
+
+        data[layer] = color_change + data[layer]
+        Logger.log("d", "Added filament change before layer " + str(layer-1))
+
+        if not finalchange:
+            Logger.log("d", "Skipping addition of final filament change.")
+            return data
+
+        final_change = """
+
+
+; Filament change code by PrusaFilamentChange
+G1 X220 Y-3.0 ; Go outside print area
+M140 S0 ; Turn off heatbed, since we're done printing
+M600 ; Filament change
+
+
+"""
+
+        # The final "layer" is Cura's End G-code
+        data[-1] = final_change + data[-1]
+        Logger.log("d", "Added final filament change.")
+        return data
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # cura-scripts
-A collection of post-processing scripts for Ultimaker Cura
+This is meant to become a collection of post-processing scripts for Ultimaker Cura, doing [G-code](https://en.wikipedia.org/wiki/G-code) modification of sliced models before 3D printing. At the moment, there's only one: `PrusaFilamentChange.py`. 
+
+![A dialog box showing Prusa Filament Change in Cura, with the Enabled option checked, Layer 26, and the Change after final layer option not checked.](/resources/PrusaFilamentChange.png?raw=true "PrusaFilamentChange")
+
+## PrusaFilamentChange
+This is meant to compensate for a poorly behaving filament change operation on the Prusa MK3s+. The MK3s+ implements the [M600 filament change](https://reprap.org/wiki/G-code#M600:_Filament_change_pause) operation to include an extrusion after the filament change is complete, followed by an immediate return to the printing location. If you need your first layer after filament change to be clean, dragging in some just-extruded plastic doesn't help.
+
+`PrusaFilamentChange.py` is a G-code modification script, similar to Cura's Filament Change. It doesn't ask for most settings, since RepRap firmware ignores parameters to M600. Instead, it generates a sequence of operations at the specified layer to leave the print head paused away from the print after a filament change:
+> G60 S2 ; Save current print position
+> G1 X220 Y-3.0 ; Go outside print area
+> M600 ; Filament change
+> M0 Remove extrusion then continue ; Pause the print
+> G1 R2 ; Return to saved print position
+
+To use, remove the extra extrusion after filament change pause, then resume. If you're printing over USB (e.g. OctoPrint), this won't be on the printer's UI.
+
+If you want to include a filament swap back at the end of the print, that's also an option. Check the box, and the last thing your print will do is a filament change.
+
+## Installation
+Cura indexes post-processing scripts from its internal scripts directory and user's script directory. On macOS, that's ~/Library/Application Support/cura/<version>/scripts/. On Windows, that's \AppData\Roaming\cura\scripts.
+
+To install a script, download it, place it in that directory, and \[re\]start Cura. Keep in mind scripts are fully functional software - you should review scripts before installing ones you don't trust!
